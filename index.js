cee105b26ee7300dc2fefd21de8abb4ea2edcfd9
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-03-15 15:15:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const canvas = require('discord-canvas')
 
 // Cria um cliente de inicialização da BOT
 const start = (kill = new Client()) => {
-    console.log(color('\n[DEV]', 'red'), color('- Lucas R. - KillovSky <-> +55 18 99804-4132 <-> https://chat.whatsapp.com/H53MdwhtnRf7TGX1VJ2Jje'))
+    console.log(color('\n[DEV]', 'red'), color('- Lucas R. - KillovSky <-> +55 81 99620-7906 <-> https://chat.whatsapp.com/LEHYF2AmnOk7MSH1r03xBB'))
 	console.log(color('[ÍRIS]', 'red'), color('Minha inicialização foi concluída, você pode usar agora...\n'))
 	
 		// Forçar recarregamento caso obtenha erros
 		kill.onStateChanged((state) => {
-			console.log('[Estado da Íris]', state)
+			console.log('[Estado da akame]', state)
 			if (state === 'UNPAIRED' || state === 'CONFLICT' || state === 'UNLAUNCHED') kill.forceRefocus()
 		})
 		
